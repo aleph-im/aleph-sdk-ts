7180e1e1ded67323318ccc89efcd9270e14f8f24
Luca Georges Francois <luca.georges-francois@epitech.eu>
2021-09-22 11:47:53 +0200

    Add solana account test
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- tests/accounts/solana.test.ts
+++ tests/accounts/solana.test.ts
@@ -1,5 +1,7 @@
 import * as solanajs from '@solana/web3.js';
-import { accounts } from '../../src/index';
+import {accounts, DEFAULT_API_V2, post} from '../../src/index';
+import {ChainType} from "../../src/accounts/account";
+import {StorageEngine} from "../../src/messages/message";
 
 describe('Solana accounts', () => {
     it('should create a new solana accounts', () => {
@@ -15,5 +17,36 @@ describe('Solana accounts', () => {
 
         expect(account.address).not.toBe('');
         expect(account.publicKey).toBe(keyPair.publicKey.toString());
+    });
+
+    it('should publish a post message correctly', async () => {
+        const account = accounts.solana.NewAccount();
+        const content: { body: string } = {
+            body: 'Hello World',
+        };
+
+        await post.Publish({
+            APIServer: DEFAULT_API_V2,
+            chain: ChainType.Solana,
+            channel: 'TEST',
+            inlineRequested: true,
+            storageEngine: StorageEngine.IPFS,
+            account: account,
+            postType: 'custom_type',
+            content: content,
+        });
+
+        const amends = await post.Get({
+            types: 'custom_type',
+            APIServer: DEFAULT_API_V2,
+            pagination: 200,
+            page: 1,
+            refs: [],
+            addresses: [],
+            tags: [],
+            hashes: [],
+        });
+
+        expect(amends.posts[0].content).toStrictEqual(content);
     });
 });
