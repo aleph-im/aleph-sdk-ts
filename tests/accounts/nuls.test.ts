5ec138600795be537c160610f575869cd3bbe2da
Roman Gascoin <54111474+Rgascoin@users.noreply.github.com>
2022-01-03 14:14:55 +0100

    Remove NULS test

--- tests/accounts/nuls.test.ts
+++ tests/accounts/nuls.test.ts
@@ -1,8 +1,6 @@
 import * as bip39 from "bip39";
-import { nuls, post } from "../index";
+import { nuls } from "../index";
 import { ChainType } from "../../src/accounts/account";
-import { DEFAULT_API_V2 } from "../../src/global";
-import { StorageEngine } from "../../src/messages/message";
 
 describe("NULS accounts", () => {
     it("should create a NULS accounts", async () => {
@@ -30,37 +28,5 @@ describe("NULS accounts", () => {
         expect(account.address).not.toBe("");
         expect(account.publicKey).toBe("02a7e23f579821364bf186b2ee0fb2aa9e5faa57cd4f281599ca242d8d9faa8533");
         expect(account.address).toBe("6HgcLR5Yjc7yyMiteQZxTpuB6NYRiqWf");
-    });
-
-    it("should publish a post message correctly", async () => {
-        const { account } = await nuls.NewAccount();
-        const content: { body: string } = {
-            body: "NULS post test 12",
-        };
-
-        const msg = await post.Publish({
-            APIServer: DEFAULT_API_V2,
-            channel: "TEST",
-            inlineRequested: true,
-            storageEngine: StorageEngine.IPFS,
-            account: account,
-            postType: "chat",
-            content: content,
-        });
-
-        expect(msg.item_hash).not.toBeNull();
-
-        const amends = await post.Get({
-            types: "chat",
-            APIServer: DEFAULT_API_V2,
-            pagination: 200,
-            page: 1,
-            refs: [],
-            addresses: [],
-            tags: [],
-            hashes: [msg.item_hash],
-        });
-
-        expect(amends.posts[0].content).toStrictEqual(content);
     });
 });
