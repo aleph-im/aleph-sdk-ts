7610ccd2b7846715a4c1f04fe01d3932f68a7385
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-08-11 11:24:55 +0200

    ref: use cosmos helper to generate a new account

--- src/accounts/cosmos.ts
+++ src/accounts/cosmos.ts
@@ -1,15 +1,14 @@
 import { DirectSecp256k1HdWallet, DirectSecp256k1HdWalletOptions, DirectSecp256k1Wallet } from "@cosmjs/proto-signing";
-import * as bip39 from "bip39";
 import { Account } from "./account";
 import { GetVerificationBuffer } from "../messages";
 import { BaseMessage, Chain } from "../messages/message";
 
-type DirectWallet = DirectSecp256k1Wallet | DirectSecp256k1HdWallet;
+type AnyDirectWallet = DirectSecp256k1Wallet | DirectSecp256k1HdWallet;
 
 export class CosmosAccount extends Account {
-    private wallet: DirectWallet;
+    private wallet: AnyDirectWallet;
 
-    constructor(wallet: DirectWallet, publicKey: string, address: string) {
+    constructor(wallet: AnyDirectWallet, publicKey: string, address: string) {
         super(address, publicKey);
         this.wallet = wallet;
     }
@@ -24,16 +23,23 @@ export class CosmosAccount extends Account {
     }
 }
 
-async function getAddressAndPublicKey(wallet: DirectWallet) {
+async function getCosmosAccount(wallet: AnyDirectWallet) {
     const [account] = await wallet.getAccounts();
     const publicKey = Buffer.from(account.pubkey).toString("hex");
 
-    return { address: account.address, publicKey };
+    return new CosmosAccount(wallet, publicKey, account.address);
 }
 
-export async function NewAccount(): Promise<CosmosAccount> {
-    const mnemonic = bip39.generateMnemonic();
-    return ImportAccountFromMnemonic(mnemonic);
+export async function NewAccount(
+    length?: 12 | 15 | 18 | 21 | 24,
+    options?: Partial<DirectSecp256k1HdWalletOptions>,
+): Promise<{ account: CosmosAccount; mnemonic: string }> {
+    const wallet = await DirectSecp256k1HdWallet.generate(length, options);
+
+    return {
+        account: await getCosmosAccount(wallet),
+        mnemonic: wallet.mnemonic,
+    };
 }
 
 export async function ImportAccountFromMnemonic(
@@ -41,15 +47,13 @@ export async function ImportAccountFromMnemonic(
     options?: Partial<DirectSecp256k1HdWalletOptions>,
 ): Promise<CosmosAccount> {
     const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, options);
-    const { address, publicKey } = await getAddressAndPublicKey(wallet);
 
-    return new CosmosAccount(wallet, publicKey, address);
+    return getCosmosAccount(wallet);
 }
 
 export async function ImportAccountFromPrivateKey(privateKey: string, prefix?: string): Promise<CosmosAccount> {
     const key = Buffer.from(privateKey);
     const wallet = await DirectSecp256k1Wallet.fromKey(key, prefix);
-    const { address, publicKey } = await getAddressAndPublicKey(wallet);
 
-    return new CosmosAccount(wallet, publicKey, address);
+    return getCosmosAccount(wallet);
 }
