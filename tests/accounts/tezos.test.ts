a417895564ed3720cfc58fce00dade39becd2e25
Mike Hukiewitz <m.h.hukiewitz@students.uu.nl>
2022-08-11 10:21:20 +0200

    add Tezos test

---
+++ tests/accounts/tezos.test.ts
@@ -0,0 +1,55 @@
+import { ItemType } from "../../src/messages/message";
+import { post, tezos } from "../index";
+import { DEFAULT_API_V2 } from "../../src/global";
+import { b58cencode, prefix } from "@taquito/utils";
+import nacl from "tweetnacl";
+
+describe("Tezos accounts", () => {
+    it("should create a new tezos accounts", async () => {
+        const { account } = await tezos.NewAccount();
+
+        expect(account.address).not.toBe("");
+        expect(account.publicKey).not.toBe("");
+    });
+
+    it("should import an tezos accounts using a private key", async () => {
+        const keyPair = nacl.sign.keyPair();
+        const secretKey = b58cencode(keyPair.secretKey, prefix.edsk);
+        const account = await tezos.ImportAccountFromPrivateKey(secretKey);
+
+        expect(account.address).not.toBe("");
+        expect(account.publicKey).toBe(b58cencode(keyPair.publicKey, prefix.edpk));
+    });
+
+    it("should publish a post message correctly", async () => {
+        const { account } = await tezos.NewAccount();
+        const content: { body: string } = {
+            body: "Hello World TEZOS",
+        };
+
+        const msg = await post.Publish({
+            APIServer: DEFAULT_API_V2,
+            channel: "ALEPH-TEST",
+            inlineRequested: true,
+            storageEngine: ItemType.ipfs,
+            account: account,
+            postType: "custom_type",
+            content: content,
+        });
+
+        expect(msg.item_hash).not.toBeUndefined();
+        console.log(msg);
+        const amends = await post.Get({
+            types: "custom_type",
+            APIServer: DEFAULT_API_V2,
+            pagination: 200,
+            page: 1,
+            refs: [],
+            addresses: [],
+            tags: [],
+            hashes: [msg.item_hash],
+        });
+        expect(amends.posts.length).not.toBe(0);
+        expect(amends.posts[0].content).toStrictEqual(content);
+    });
+});
--- tests/index.ts
+++ tests/index.ts
@@ -3,6 +3,7 @@ import * as cosmos from "../src/accounts/cosmos";
 import * as ethereum from "../src/accounts/ethereum";
 import * as nuls2 from "../src/accounts/nuls2";
 import * as solana from "../src/accounts/solana";
+import * as tezos from "../src/accounts/tezos";
 
 import * as aggregate from "../src/messages/aggregate/index";
 import * as post from "../src/messages/post/index";
@@ -10,4 +11,4 @@ import * as store from "../src/messages/store/index";
 import * as forget from "../src/messages/forget/index";
 import * as program from "../src/messages/program/index";
 
-export { avalanche, cosmos, ethereum, nuls2, solana, aggregate, post, store, forget, program };
+export { avalanche, cosmos, ethereum, nuls2, solana, tezos, aggregate, post, store, forget, program };
