3c0a33f99c5e174bcf6aa335fae3ef59b7791080
Roman Gascoin <roman.gascoin@gmail.com>
2022-09-19 16:23:22 +0200

    Drop Nuls support (#43)
    * Refactor: NULS is no longer used by Aleph.
    
    Solution: Remove the possibility to create NULS Account with the SDK.

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,6 @@
 import * as ethereum from "../src/accounts/ethereum";
 import * as solana from "../src/accounts/solana";
 import * as nuls2 from "../src/accounts/nuls2";
-import * as nuls from "../src/accounts/nuls";
 import * as avalanche from "../src/accounts/avalanche";
 
 import * as aggregate from "../src/messages/aggregate/index";
@@ -10,4 +9,4 @@ import * as store from "../src/messages/store/index";
 import * as forget from "../src/messages/forget/index";
 import * as program from "../src/messages/program/index";
 
-export { avalanche, ethereum, solana, nuls, nuls2, aggregate, post, store, forget, program };
+export { avalanche, ethereum, solana, nuls2, aggregate, post, store, forget, program };
