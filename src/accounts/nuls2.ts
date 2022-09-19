6265efea74c6564e9e511c053eaf7c6ffc9e1f86
Roman Gascoin <roman.gascoin@gmail.com>
2022-09-19 16:23:22 +0200

    Drop Nuls support (#43)
    * Refactor: NULS is no longer used by Aleph.
    
    Solution: Remove the possibility to create NULS Account with the SDK.

--- src/accounts/nuls2.ts
+++ src/accounts/nuls2.ts
@@ -5,8 +5,15 @@ import { generateMnemonic } from "bip39";
 import { Account } from "./account";
 import { BaseMessage, Chain } from "../messages/message";
 import { GetVerificationBuffer } from "../messages";
-import { addressFromHash, magicHash, privateKeyToPublicKey, publicKeyToHash } from "./nuls";
 import { decrypt as secp256k1_decrypt, encrypt as secp256k1_encrypt } from "eciesjs";
+import bs58 from "bs58";
+import shajs from "sha.js";
+import RIPEMD160 from "ripemd160";
+
+export type ChainNAddress = {
+    chain_id?: number;
+    address_type?: number;
+};
 
 export type NULS2ImportConfig = {
     chain_id?: number;
@@ -58,7 +65,7 @@ export class NULS2Account extends Account {
      * @param message The Aleph message to sign, using some of its fields.
      */
     Sign(message: BaseMessage): Promise<string> {
-        const digest = magicHash(GetVerificationBuffer(message));
+        const digest = NULS2Account.magicHash(GetVerificationBuffer(message));
         const privateKeyBuffer = Buffer.from(this.privateKey, "hex");
 
         return new Promise((resolve) => {
@@ -78,6 +85,70 @@ export class NULS2Account extends Account {
     private EncodeSignature(signature: Uint8Array, recovery: number, compressed: boolean) {
         if (compressed) recovery += 4;
         return Buffer.concat([Buffer.alloc(1, recovery + 27), signature]);
+    }
+
+    /**
+     * Extract an address from a given hash.
+     *
+     * @param hash The hash containing the address.
+     * @param prefix The optional address prefix.
+     */
+    static addressFromHash(hash: Uint8Array, prefix?: string): string {
+        const xoredHash = hash.reduce((xor, i) => (xor ^= i), 0);
+        const address = bs58.encode(Buffer.concat([hash, Buffer.from([xoredHash])]));
+
+        if (prefix) return prefix + String.fromCharCode(prefix.length + 96) + address;
+        return address;
+    }
+
+    /**
+     * Creates a hash from a user's public key.
+     *
+     * @param publicKey The public key used to create the hash.
+     * @param chain_id The optional chain id.
+     * @param address_type The optional address type.
+     */
+    static publicKeyToHash(
+        publicKey: Uint8Array,
+        { chain_id = 8964, address_type = 1 }: ChainNAddress = { chain_id: 8964, address_type: 1 },
+    ): Buffer {
+        const sha = new shajs.sha256().update(publicKey).digest();
+        const publicKeyHash = new RIPEMD160().update(sha).digest();
+        const output = Buffer.allocUnsafe(3);
+
+        output.writeInt16LE(chain_id, 0);
+        output.writeInt8(address_type, 2);
+        return Buffer.concat([output, publicKeyHash]);
+    }
+
+    /**
+     * Creates a hash from a message.
+     *
+     * @param message The message used to create the hash.
+     * @param messagePrefix The optional message's hash prefix.
+     */
+    static magicHash(message: Buffer, messagePrefix: string | Buffer = "\u0018NULS Signed Message:\n"): Buffer {
+        if (!Buffer.isBuffer(messagePrefix)) messagePrefix = Buffer.from(messagePrefix);
+
+        let buffer = Buffer.allocUnsafe(messagePrefix.length + 6 + message.length);
+        let cursor = messagePrefix.copy(buffer, 0);
+        if (message.length < 253) {
+            buffer[cursor] = message.length;
+            cursor += 1;
+        } else if (message.length <= 0xffff) {
+            buffer[cursor] = 253;
+            buffer.writeUIntLE(message.length, cursor + 1, 2);
+            cursor += 3;
+        } else if (message.length <= 0xffffffff) {
+            buffer[cursor] = 254;
+            buffer.writeUIntLE(message.length, cursor + 1, 4);
+            cursor += 5;
+        } else {
+            throw new Error("Message can't be hashed due to is size");
+        }
+        cursor += Buffer.from(message).copy(buffer, cursor);
+        buffer = buffer.slice(0, cursor);
+        return new shajs.sha256().update(buffer).digest();
     }
 }
 
@@ -132,10 +203,10 @@ export async function ImportAccountFromPrivateKey(
     privateKey: string,
     { chain_id = 1, prefix = "NULS" }: NULS2ImportConfig = { chain_id: 1, prefix: "NULS" },
 ): Promise<NULS2Account> {
-    const pub = privateKeyToPublicKey(Buffer.from(privateKey, "hex"));
+    const pub = secp256k1.publicKeyCreate(Buffer.from(privateKey, "hex"));
     const publicKey = Buffer.from(pub).toString("hex");
 
-    const hash = publicKeyToHash(pub, { chain_id: chain_id });
-    const address = addressFromHash(hash, prefix);
+    const hash = NULS2Account.publicKeyToHash(pub, { chain_id: chain_id });
+    const address = NULS2Account.addressFromHash(hash, prefix);
     return new NULS2Account(address, publicKey, privateKey);
 }
