c0e3d89a8ece45cc914d3b75714a9aaa55541b71
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-09-21 11:48:00 +0200

    Add a react application example (#40)
    * feat: Toolshed example
    
    The Toolshed is a small react application to test Aleph POST messages, from all available Keypairs/Wallets.
    
    * add: import from private key or mnemonic
    
    * fix: modules do not implement hasOwnProperty
    
    * chore: rm NULS 1 from toolshed

--- examples/send-message-with-phantom/src/getPhantom.ts
+++
@@ -1,9 +0,0 @@
-export const getPhantom = () => {
-    if ("phantom" in window) {
-        const provider = window.phantom?.solana;
-
-        if (provider?.isPhantom) {
-            return provider;
-        }
-    }
-};
---
+++ examples/toolshed/src/components/MessageConfig.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+import { ItemType } from "../../../../src/messages/message";
+import { Publish } from "../../../../src/messages/post";
+import { DEFAULT_API_V2 } from "../../../../src/global";
+import { consumeProps } from "../model/componentProps";
+
+function MessageConfig({ state }: consumeProps) {
+  const [messageHash, setMessageHash] = useState(null)
+  const [isSending, setIsSending] = useState(false)
+  const [messageContent, setMessageContent] = useState("Did the quick brown fox jump over the lazy dog?!")
+
+  const handleChange = ( e: any ) => {
+   setMessageContent(e.target.value)
+  }
+
+  const sendMessage = async () => {
+    setMessageHash(null)
+    setIsSending(true)
+
+    const message = await Publish({
+        APIServer: DEFAULT_API_V2,
+        channel: "Typescript-SDK-Toolshed",
+        inlineRequested: true,
+        storageEngine: ItemType.ipfs,
+        account: state.account,
+        postType: "Toolshed",
+        content: messageContent,
+    })
+
+    setMessageHash(message.item_hash)
+    setIsSending(false)
+  }
+
+  return (
+    <>
+    <div>
+      <input value={messageContent} 
+            size={55}
+            onChange={handleChange} />
+      
+      <button onClick={sendMessage} disabled={isSending}>Send</button>
+    </div>
+    {
+      isSending && <p>Sending your message</p>
+    }
+    {
+      messageHash &&
+      <p>
+        Your message was succesfully posted! <a href={`https://explorer.aleph.im/address/${state.account?.GetChain()}/${state.account?.address}/message/POST/${messageHash}`} target="_blank">Check on explorer</a>
+      </p>
+    }
+    </>
+  )
+}
+
+export default MessageConfig
