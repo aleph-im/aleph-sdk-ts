0714d4de3ef474882536949fb86ee08b70fdfca1
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-09-23 14:50:53 +0200

    Add support for cosmos messages  (#28)
    * init: cosmos
    
    * ref: use cosmos helper to generate a new account
    
    * add: sign method
    
    * chore: rm useless stargate dependency
    
    * add: tests (WIP)
    
    * fix:  StdSignature.toString(), serilization fails
    
    * ref: use amino instead of proto sign
    
    * fix: sign helper takes a string not a buffer
    
    * fix: tests
    
    * fix: tests
    
    * chore: signature redundancy

--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,8 @@
-import * as ethereum from "../src/accounts/ethereum";
-import * as solana from "../src/accounts/solana";
-import * as nuls2 from "../src/accounts/nuls2";
 import * as avalanche from "../src/accounts/avalanche";
+import * as cosmos from "../src/accounts/cosmos";
+import * as ethereum from "../src/accounts/ethereum";
+import * as nuls2 from "../src/accounts/nuls2";
+import * as solana from "../src/accounts/solana";
 
 import * as aggregate from "../src/messages/aggregate/index";
 import * as post from "../src/messages/post/index";
@@ -9,4 +10,4 @@ import * as store from "../src/messages/store/index";
 import * as forget from "../src/messages/forget/index";
 import * as program from "../src/messages/program/index";
 
-export { avalanche, ethereum, solana, nuls2, aggregate, post, store, forget, program };
+export { avalanche, cosmos, ethereum, nuls2, solana, aggregate, post, store, forget, program };
