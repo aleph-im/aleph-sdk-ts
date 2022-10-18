1b483919f5dd9d4e117dadfb9306632d6edb8d41
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-10-18 12:02:41 +0200

    init: Ledger support for Ethereum

---
+++ src/providers/Ledger/ethereum.ts
@@ -0,0 +1,48 @@
+import EthApp from "@ledgerhq/hw-app-eth";
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
+
+import { Account } from "../../accounts/account";
+import { Chain, BaseMessage } from "../../messages/message";
+import { GetVerificationBuffer } from "../../messages";
+
+const DERIVATION_PATH = "44'/60'/0'/0/0";
+
+class ETHLedgerAccount extends Account {
+    private signer: EthApp;
+
+    constructor(signer: EthApp, address: string) {
+        super(address);
+        this.signer = signer;
+    }
+
+    GetChain(): Chain {
+        return Chain.ETH;
+    }
+
+    private async getSignature(input: Buffer) {
+        const rsv = await this.signer.signPersonalMessage(DERIVATION_PATH, input.toString("hex"));
+        const { r, s } = rsv;
+        const v = Number(rsv.v - 27)
+            .toString(16)
+            .padStart(2, "0");
+
+        return `0x${r}${s}${v}`;
+    }
+
+    async Sign(message: BaseMessage): Promise<string> {
+        const buf = GetVerificationBuffer(message);
+        return this.getSignature(buf);
+    }
+}
+
+/**
+ * This method retrieves an Ethereum account from a connected Ledger device
+ */
+export async function GetAccountFromLedger(): Promise<ETHLedgerAccount> {
+    const transport = await TransportWebUSB.create();
+    const signer = new EthApp(transport);
+
+    const { address } = await signer.getAddress(DERIVATION_PATH);
+
+    return new ETHLedgerAccount(signer, address);
+}
