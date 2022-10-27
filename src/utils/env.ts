68254ab5264ab356fb34d46adb91ca678d338f35
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-10-27 16:52:24 +0200

    enh: environment detection

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,20 @@
+/**
+ * Detects if the script is running inside Node.js or the browser. Checks
+ * if there is Node version in the environnement variable
+ * AND
+ * if window is already defined
+ */
 export function isNode(): boolean {
-    return process?.version !== undefined;
+    const hasNodeVersion = process?.version !== undefined;
+    let hasNoWindow = true;
+
+    try {
+        // should fail in strict mode
+        const window = {};
+        window;
+    } catch (error) {
+        hasNoWindow = false;
+    }
+
+    return hasNodeVersion && hasNoWindow;
 }
