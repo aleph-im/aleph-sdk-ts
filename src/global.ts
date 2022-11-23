a3f78f511c29469d0e1851969ffaf04d93def0db
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-11-23 10:15:27 +0100

    add: RequireAtLeastOne type

--- src/global.ts
+++ src/global.ts
@@ -1,1 +1,5 @@
 export const DEFAULT_API_V2 = "https://api2.aleph.im";
+
+export type RequireAtLeastOne<T> = {
+    [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>;
+}[keyof T];
