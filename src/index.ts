d49bb5e1b3c2b3606b7db71a741f53bec294e27b
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-11-24 12:22:55 +0100

    ref: `messages` export path (#101)
    Typedoc wasn't working properly with the previous namespace export, should work now

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as accounts from "./accounts/index";
-import { messages } from "./messages";
+import * as messages from "./messages";
 import * as Ledger from "./accounts/providers/Ledger";
 
 export { accounts, Ledger, messages };
--- src/messages/index.ts
+++ src/messages/index.ts
@@ -5,7 +5,7 @@ import * as post from "./post";
 import * as program from "./program";
 import * as store from "./store";
 
-export const messages = { aggregate, forget, post, program, store };
+export { aggregate, forget, post, program, store };
 
 /**
  * Extracts some fields from an Aleph message to sign it using an account.
