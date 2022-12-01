238ec05ad717ca6b38394ce0f245b212d56d6407
Bonjour Internet <BjrInt@users.noreply.github.com>
2022-12-01 10:35:41 +0100

    add: test case for store

--- tests/messages/store/get.test.ts
+++ tests/messages/store/get.test.ts
@@ -17,4 +17,15 @@ describe("Store message retrieval", () => {
 
         expect(got).toBe(expected);
     });
+
+    it("should retrieve a store message without an APIServer argument", async () => {
+        const response = await store.Get({
+            fileHash: "QmQkv43jguT5HLC8TPbYJi2iEmr4MgLgu4nmBoR4zjYb3L",
+        });
+
+        const got = ArraybufferToString(response);
+        const expected = "This is just a test.";
+
+        expect(got).toBe(expected);
+    });
 });
