2b1549f78c2733e3e887fb9bb149feaed0563855
Roman Gascoin <roman.gascoin@gmail.com>
2023-01-27 05:41:41 -0800

    Feat: user had to publish the source code to spawn an Aleph VM. (#118)
    Solution: Add a Spawn method to launch VM by only giving a codebase reference.

---
+++ src/messages/program/spawn.ts
@@ -0,0 +1,73 @@
+import { Account } from "../../accounts/account";
+import { ItemType, ProgramMessage } from "../message";
+import { Encoding, MachineVolume } from "./programModel";
+import { DEFAULT_API_V2 } from "../../global";
+import { publish } from "./publish";
+
+/**
+ * account:         The account used to sign the aggregate message.
+ *
+ * channel:         The channel in which the message will be published.
+ *
+ * storageEngine:   The storage engine to used when storing the message in case of Max_size (IPFS or Aleph storage).
+ *
+ * inlineRequested: If set to False, the Program message will be store on the same storageEngine you picked.
+ *
+ * APIServer:       The API server endpoint used to carry the request to the Aleph's network.
+ *
+ * file:            The source code of the program in under Zip format.
+ *
+ * entrypoint:      The entrypoint of your program.
+ *
+ * Subscription:    How to start you program? default by Http.
+ *
+ * memory:          Memory amount.
+ *
+ * runtime:         The docker image to use for the program.
+ *
+ * volumes:         mount point to use for storage.
+ */
+type ProgramSpawnConfiguration = {
+    account: Account;
+    channel: string;
+    storageEngine?: ItemType.ipfs | ItemType.storage;
+    inlineRequested?: boolean;
+    APIServer?: string;
+    programRef: string;
+    entrypoint: string;
+    encoding?: Encoding;
+    subscription?: Record<string, unknown>[];
+    memory?: number;
+    runtime?: string;
+    volumes?: MachineVolume[];
+};
+
+export async function Spawn({
+    account,
+    channel,
+    inlineRequested = true,
+    storageEngine = ItemType.ipfs,
+    APIServer = DEFAULT_API_V2,
+    programRef,
+    entrypoint,
+    encoding = Encoding.zip,
+    subscription,
+    memory = 128,
+    runtime = "bd79839bf96e595a06da5ac0b6ba51dea6f7e2591bb913deccded04d831d29f4",
+    volumes = [],
+}: ProgramSpawnConfiguration): Promise<ProgramMessage> {
+    return await publish({
+        account,
+        channel,
+        inlineRequested,
+        storageEngine,
+        APIServer,
+        programRef,
+        entrypoint,
+        encoding,
+        subscription,
+        memory,
+        runtime,
+        volumes,
+    });
+}
