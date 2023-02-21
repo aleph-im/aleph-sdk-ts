868efebadfcc80d8a298fa74f365761eba5b53a5
Rgascoin <roman.gascoin@gmail.com>
2023-02-21 15:57:45 +0100

    Fix: Update toolshade with the new return format from encrypt with a provider

--- examples/toolshed/src/components/EncryptionConfig.tsx
+++ examples/toolshed/src/components/EncryptionConfig.tsx
@@ -5,7 +5,7 @@ import { ECIESAccount } from "../../../../src/accounts/account";
 
 type EncryptionFormType = {
     rawMessage: string;
-    encryptedMessage: string;
+    encryptedMessage: string | Buffer;
     decryptedMessage: string;
 };
 
@@ -27,13 +27,13 @@ function EncryptionConfig({ state }: consumeProps) {
     if (!(state.account instanceof ECIESAccount)) return;
 
     const encrypted = await state.account.encrypt(Buffer.from(encryptionContent.rawMessage))
-    setEncryptionContent({...encryptionContent, encryptedMessage: encrypted.toString("hex")})
+    setEncryptionContent({...encryptionContent, encryptedMessage: encrypted})
   }
 
   const decryptMessage = async () => {
     if (!(state.account instanceof ECIESAccount) || !encryptionContent.encryptedMessage) return;
 
-    const decrypted = await state.account.decrypt(Buffer.from(encryptionContent.encryptedMessage, "hex"))
+    const decrypted = await state.account.decrypt(encryptionContent.encryptedMessage)
     setEncryptionContent({...encryptionContent, decryptedMessage: decrypted.toString()})
   }
 
@@ -52,7 +52,7 @@ function EncryptionConfig({ state }: consumeProps) {
       <button onClick={async () => await cryptMessage()}>Encrypt</button>
     </div>
     <div>
-        <input value={encryptionContent.encryptedMessage.toString()}
+        <input value={encryptionContent.encryptedMessage.toString('hex')}
                size={55}
                onChange={handleChange}
                name="encryptedMessage"
