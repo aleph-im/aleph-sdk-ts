e297c8be6c7143b239d41eb3b8592ccc64064802
Roman Gascoin <roman.gascoin@gmail.com>
2023-03-13 11:51:23 +0100

    Feat: Add wrapped protocol implementation for Matic and BSC (#123)

--- examples/toolshed/src/components/WalletConfig.tsx
+++ examples/toolshed/src/components/WalletConfig.tsx
@@ -1,11 +1,27 @@
-import {avalanche, ethereum, solana} from '../../../../src/accounts'
-import {WalletChains} from '../model/chains'
-import {dispatchAndConsume} from '../model/componentProps'
-import {Actions} from '../reducer'
-import {ChangeRpcParam, RpcChainType} from "../../../../src/accounts/providers/JsonRPCWallet";
+import { avalanche, ethereum, solana } from '../../../../src/accounts'
+import { WalletChains } from '../model/chains'
+import { dispatchAndConsume } from '../model/componentProps'
+import { Actions } from '../reducer'
+import { RpcChainType } from "../../../../src/accounts/providers/JsonRPCWallet";
+import Select, { SingleValue } from "react-select";
+import { useState } from "react";
 
+type Option = {
+  readonly label: string
+  readonly value: number
+  readonly isDisabled?: boolean
+}
+
+const availableChains: Option[] = [
+  { label: 'Ethereum Mainnet', value: RpcChainType.ETH },
+  { label: 'Ethereum Mainnet (FLASHBOT)', value: RpcChainType.ETH_FLASHBOTS },
+  { label: 'Avalanche Mainnet', value: RpcChainType.AVAX },
+  { label: 'Polygon Mainnet', value: RpcChainType.POLYGON },
+  { label: 'BSC Mainnet', value: RpcChainType.BSC },
+]
 
 function WalletConfig({ dispatch, state } : dispatchAndConsume) {
+  const [customEndpoint, setCustomEndpoint] = useState<RpcChainType>(availableChains[0].value)
   const getAccountClass = () => (
     state.selectedChain === WalletChains.Avalanche ? [avalanche, window.ethereum]
     : state.selectedChain === WalletChains.Ethereum ? [ethereum, window.ethereum]
@@ -13,14 +29,14 @@ function WalletConfig({ dispatch, state } : dispatchAndConsume) {
     : [null, null]
   )
 
-  const connectToMetamask = async (endpoint?: ChangeRpcParam) => {
+  const connectToMetamask = async () => {
     const [_account, provider] = getAccountClass();
 
     if(_account === null)
       return 
 
     try{
-      const account = endpoint ? await _account.GetAccountFromProvider(provider, endpoint) :
+      const account = state.selectedChain === WalletChains.Ethereum ? await _account.GetAccountFromProvider(provider, customEndpoint) :
           await _account.GetAccountFromProvider(provider)
       dispatch({
         type: Actions.SET_ACCOUNT,
@@ -32,15 +48,23 @@ function WalletConfig({ dispatch, state } : dispatchAndConsume) {
     }
   }
 
+  const handleChange = (x: SingleValue<Option> ) => {
+    if (!!x) setCustomEndpoint(x.value);
+  }
+
   return (
-    <div>
-      <button onClick={async () => {await connectToMetamask()}}>Connect to Provider</button>
-      {state.selectedChain === WalletChains.Ethereum && <button style={{'marginLeft': '4px'}}
-          onClick={async () =>
-            {await connectToMetamask(RpcChainType.ETH_FLASHBOTS);
-      }}>
-        Connect to Provider (Flashbots endpoint)
-      </button> }
+    <div style={{display: 'flex', gap: '12px'}}>
+      <button
+        onClick={async () => {await connectToMetamask()}}>
+        Connect to Provider
+      </button>
+      {state.selectedChain === WalletChains.Ethereum && (
+          <div>
+            <Select options={availableChains}
+                    defaultValue={availableChains[0]}
+                    onChange={handleChange} />
+          </div>
+      )}
     </div>
   )
 }
