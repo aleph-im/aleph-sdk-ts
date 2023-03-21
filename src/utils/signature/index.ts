746b21ed0faf05744064f12c6fc9598af2c2bb53
Rgascoin <roman.gascoin@gmail.com>
2023-03-21 19:12:48 +0100

    Feat: Add signature verification for Tezos inside the ts sdk

--- src/utils/signature/index.ts
+++ src/utils/signature/index.ts
@@ -2,5 +2,6 @@ import { verifEthereum } from "./verifEthereum";
 import { verifAvalanche } from "./verifAvalanche";
 import { verifSolana } from "./verifSolana";
 import { verifSubstrate } from "./verifSubstrate";
+import { verifTezos } from "./verifTezos";
 
-export { verifEthereum, verifAvalanche, verifSolana, verifSubstrate };
+export { verifEthereum, verifAvalanche, verifSolana, verifSubstrate, verifTezos };
