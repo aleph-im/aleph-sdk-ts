e626bc3d981ad6a0d64ddefe1a4ebb0abe5f082a
leirbag95 <gabrielelfassi@yahoo.fr>
2023-06-08 15:29:37 +0200

    verifCosmos script added without test

--- src/utils/signature/index.ts
+++ src/utils/signature/index.ts
@@ -3,5 +3,6 @@ import { verifAvalanche } from "./verifAvalanche";
 import { verifSolana } from "./verifSolana";
 import { verifSubstrate } from "./verifSubstrate";
 import { verifTezos } from "./verifTezos";
+import { verifCosmos } from "./verifCosmos";
 
-export { verifEthereum, verifAvalanche, verifSolana, verifSubstrate, verifTezos };
+export { verifEthereum, verifAvalanche, verifSolana, verifSubstrate, verifTezos, verifCosmos };
