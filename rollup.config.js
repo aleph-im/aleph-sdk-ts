1113b871698ead37c38928a4ee5f2bc77103002a
mhh <mike.hukiewitz@robotter.ai>
2024-03-07 15:13:49 +0100

    Improve declaration files in bundle

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,8 @@ import terser from '@rollup/plugin-terser'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import json from '@rollup/plugin-json'
 import nodePolyfills from 'rollup-plugin-polyfill-node'
+import { dts } from 'rollup-plugin-dts'
+import del from 'rollup-plugin-delete'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
@@ -21,12 +23,6 @@ export default [
   {
     input: 'src/index.ts',
     output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-        inlineDynamicImports: true,
-      },
       {
         file: packageJson.module,
         format: 'esm',
@@ -52,6 +48,44 @@ export default [
         inlineSources: false,
       }),
       terser(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        inlineDynamicImports: true,
+      },
+    ],
+    plugins: [
+      json(),
+      peerDepsExternal({
+        includeDependencies: true,
+      }),
+      resolve({
+        extensions: ['.ts', '.js', '.mjs'],
+        preferBuiltins: true,
+        browser: false,
+      }),
+      commonjs(),
+      typescript({
+        tsconfig,
+        sourceMap: true,
+        inlineSources: false,
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [
+      dts({ tsconfig }),
+      del({ targets: 'dist/esm/types', hook: 'buildEnd' }),
+      del({ targets: 'dist/cjs/types', hook: 'buildEnd' }),
     ],
   },
 ]
