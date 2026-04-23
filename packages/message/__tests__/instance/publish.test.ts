5fc93cd67eb962838cc0f51635d8e3a2dc33c38a
Olivier Desenfans <desenfans.olivier@gmail.com>
2026-04-23 17:19:42 +0200

    chore: fix import order in instance test

--- packages/message/__tests__/instance/publish.test.ts
+++ packages/message/__tests__/instance/publish.test.ts
@@ -1,6 +1,5 @@
-import axios from 'axios'
-
 import { Blockchain } from '@aleph-sdk/core'
+import axios from 'axios'
 
 import * as ethereum from '../../../ethereum/src'
 import { InstanceMessageClient, MAXIMUM_DISK_SIZE, PaymentType } from '../../src'
